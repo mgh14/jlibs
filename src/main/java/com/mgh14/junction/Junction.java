332dbc084fd99e18a09b72e8de1a59a14eb5d767
mgh14 <chasing314@gmail.com>
2017-06-26 21:55:34 -0600

    More work on junction and maybe

--- src/main/java/com/mgh14/junction/Junction.java
+++ src/main/java/com/mgh14/junction/Junction.java
@@ -1,45 +1,74 @@
 package com.mgh14.junction;
 
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
  * TODO: Document
  */
-public class Junction<T, V> {
+public class Junction<T> {
 
     private T operand;
-    private V returnValue;
 
-    public Junction(T operand) {
-        setOperand(operand);
-        setReturnValue(null);
-    }
-
-    public Junction() {
-        this(null);
-    }
-
-    public void setOperand(T operand) {
+    protected Junction(T operand) {
         this.operand = operand;
     }
 
-    private void setReturnValue(V returnValue) {
-        this.returnValue = returnValue;
-
-
-
-
+    public static <S> Junction<S> of() {
+        return of(null);
     }
 
-    public Junction<V, Object> ifSucceeds(Predicate<Object> condition,
-                                          Function<T, V> successFunction) {
-        setReturnValue(successFunction.apply(this.operand));
-        return new Junction<>(returnValue);
+    // alias
+    public static <S> Junction<S> empty() {
+        return of();
     }
 
-    public Junction<V, Object> elseIfFails(Function<T, V> failFunction) {
-        return new Junction<>(failFunction.apply(this.operand));
+    // alias
+    /*public static <S> Junction<S> empty(Class<S> classOfValue) {
+        return of((classOfValue) null);
+    }*/
+
+    public static <S> Junction<S> of(S value) {
+        return new Junction<>(value);
+    }
+
+    public T getValue() {
+        return operand;
+    }
+
+    public Junction<T> ifSucceeds(Predicate<T> condition) {
+        return condition.test(operand) ? of(operand) : of();
+    }
+
+    public Junction<T> ifFails(Predicate<T> condition) {
+        return ifSucceeds(condition.negate());
+    }
+
+    public <V> Junction<V> doOperation(Function<T, V> function) {
+        return doOperation(true, function);
+    }
+
+    public <V> Junction<V> doOperation(boolean checkForNull, Function<T, V> function) {
+        Junction<T> startJunction = (checkForNull) ? ifSucceeds(Objects::nonNull) : this;
+        return of(function.apply(startJunction.getValue()));
+    }
+
+    public <V> Junction<V> thenDo(Function<T, V> function) {
+        return doOperation(function);
+    }
+
+    // alias
+    public <V> Junction<V> orElseDo(Function<T, V> failFunction) {
+        return doOperation(failFunction);
+    }
+
+    public <E extends Exception> Junction<T> orElseThrow_returnJunction(E exception) throws E {
+        throw exception;
+    }
+
+    public <E extends Exception> T orElseThrow_returnValue(E exception) throws E {
+        return (orElseThrow_returnJunction(exception)).getValue();
     }
 
 }
--- src/main/java/com/mgh14/maybe/Maybe.java
+++ src/main/java/com/mgh14/maybe/Maybe.java
@@ -1,18 +1,21 @@
 package com.mgh14.maybe;
 
+import com.mgh14.junction.Junction;
+
 import java.util.Objects;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 /**
  * TODO: Document
  */
-public class Maybe<T> {
+public class Maybe<T> extends Junction<T> {
+
+    private Maybe(T operand) {
+        super(operand);
+    }
 
     public static void main(String[] args) {
-        Integer x = 1;
+        /*Integer x = 1;
         Integer y = null;
 
         if (x != null) {
@@ -26,111 +29,43 @@ public class Maybe<T> {
         }
 
         Maybe<Integer> addedMe = Maybe.ofValue(x)
-                .presentThenDo(y, (integer, integer2) -> Maybe.ofValue(y)
-                        .presentThenDo(() -> integer + integer2)
+                .ifPresent(y, (integer, integer2) -> Maybe.ofValue(y)
+                        .ifPresent(() -> integer + integer2)
                         .getValue());
 
         System.out.println("Added1: " + addedMe.getValue());
 
         Integer added = Maybe.ofValue(x)
-                .presentThenDo(y, (integer, integer2) -> Maybe.ofValue(y)
-                        .presentThenDo(() -> integer + integer2)
+                .ifPresent(y, (integer, integer2) -> Maybe.ofValue(y)
+                        .ifPresent(() -> integer + integer2)
                         .getValue())
-                .orElseThrow(new IllegalArgumentException("Added integers cannot be null32"))
+                .orElseThrow_returnJunction(new IllegalArgumentException("Added integers cannot be null32"))
                 .getValue();
-        System.out.println("Added: " + added);
+        System.out.println("Added: " + added);*/
     }
 
-    private final T operand;
-
-    public Maybe(T operand) {
-        this.operand = operand;
+    @Override
+    public <V> Maybe<V> doOperation(Function<T, V> function) {
+        V value = Maybe.of(super.getValue())
+                .ifPresent()
+                .doOperation(function)
+                .orElseDo((Function<V, V>) v -> null).getValue();
+        return of(value);
     }
 
-    public Maybe() {
-        this(null);
+    @Override
+    public <V> Maybe<V> orElseDo(Function<T, V> failFunction) {
+        return Maybe.of((T) null)
+                .doOperation(failFunction);
     }
 
-    public static <V> Maybe<V> ofValue(V value) {
+    public Maybe<T> ifPresent() {
+        Junction<T> result = super.ifSucceeds(Objects::nonNull);
+        return of(result.getValue());
+    }
+
+    public static <V> Maybe<V> of(V value) {
         return new Maybe<>(value);
     }
-
-    public T getValue() {
-        return this.operand;
-    }
-
-    public <V, W> Maybe<W> presentThenDo(V secondArg, BiFunction<T, V, W> biFunction) {
-        return (this.operand != null) ? thenDo(secondArg, biFunction) :
-                ofValue(null);
-    }
-
-    public <V> Maybe<V> presentThenDo(Function<T, V> function) {
-        return presentThenDo(null, (BiFunction<T, T, V>) (t, t2) -> {
-            return function.apply(t);   // intentionally ignore arg t2,
-            // which should be null here anyways
-        });
-    }
-
-    public Maybe<T> presentThenDo(Consumer<T> consumer) {
-        return presentThenDo(null, (t, t2) -> {
-            consumer.accept(t);
-
-            return this.operand;
-        });
-    }
-
-    public <V> Maybe<V> presentThenDo(Supplier<V> supplier) {
-        return presentThenDo(null, (t, t2) -> supplier.get());
-    }
-
-    public <V, U> Maybe<U> thenDo(V secondaryArg, BiFunction<T, V, U> biFunction) {
-        return ofValue(biFunction.apply(this.operand, secondaryArg));
-    }
-
-    public <V, U> Maybe<U> elseDo(V secondaryArg, BiFunction<T, V, U> biFunction) {
-        return thenDo(secondaryArg, biFunction);
-    }
-
-    public <V> Maybe<V> thenDo(Function<T, V> failFunction) {
-        return elseDo(null, (BiFunction<T, T, V>) (t1, t2) ->
-                failFunction.apply(t1)   // intentionally ignore (null) t2
-        );
-    }
-
-    public <V> Maybe<V> elseDo(Function<T, V> failFunction) {
-        return thenDo(failFunction);
-    }
-
-    public void thenDo(Consumer<T> consumable) {
-        thenDo(null, (BiFunction<T, T, T>) (t, t2) -> {
-            consumable.accept(t);     // intentionally ignore (null) t2
-            return null;
-        });
-    }
-
-    public void elseDo(Consumer<T> failRunnable) {
-        thenDo(failRunnable);
-    }
-
-    public void thenDo(Runnable failRunnable) {
-        thenDo(null, (BiFunction<T, T, T>) (t, t2) -> {
-            failRunnable.run();     // intentionally ignore (null) t and t2
-            return null;
-        });
-    }
-
-    public void elseDo(Runnable failRunnable) {
-        thenDo(failRunnable);
-    }
-
-    public <V extends Throwable> Maybe<T> orElseThrow(V exception) throws V {
-        if (this.operand == null) {
-            Objects.requireNonNull(exception);
-            throw exception;
-        } else {
-            return this;
-        }
-    }
-
 }
 
